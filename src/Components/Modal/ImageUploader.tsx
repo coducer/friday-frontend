71ea9670265114a0049e7f41feb2f95e863ecc4e
Ayesha <ayesha@coducer.com>
2024-04-16 11:40:09 +0530

    Fix:tryon_url submit fix

--- src/Components/Modal/ImageUploader.tsx
+++ src/Components/Modal/ImageUploader.tsx
@@ -19,6 +19,8 @@ const ImageUploader = ({
   const [orderId, setOrderId]: [string, Function] = useState("");
   const [orderTryonUrl, setOrderTryonUrl]: [string, Function] = useState("");
   const [intervalId, setIntervalId]: [any, Function] = useState(null);
+  const [tryonUrlActivate, setTryonUrlActivate]: [boolean, Function] =
+    useState(false);
 
   const handleImageUpload = async (event: any) => {
     const formData = new FormData();
@@ -43,23 +45,35 @@ const ImageUploader = ({
     try {
       let response = await context?.getOrders(id);
 
-      if (response?.order) {
-        if (response?.order?.tryon_url !== null) {
-          setLoader(false);
-          clearInterval(intervalId);
-        }
-
-        setOrderTryonUrl(response?.order?.tryon_url);
-        setImageUrl(response?.order?.tryon_url);
+      if (response?.order?.garment_url) {
+        setImageUrl(response?.order?.garment_url);
+        setLoader(false);
+        setTryonUrlActivate(true);
       }
     } catch (error) {}
   };
 
   function getOrdersInterval(id: string) {
     return function () {
-      handleGetOrders(id);
+      handleTryonUrl(id);
     };
   }
+
+  const handleTryonUrl = async (id: any) => {
+    setLoader(true);
+    try {
+      let response = await context?.getOrders(id);
+      if (response?.order) {
+        if (response?.order?.tryon_url !== null) {
+          setLoader(false);
+          clearInterval(intervalId);
+        }
+        setImageUrl(response?.order?.tryon_url);
+        setTryonUrlActivate(false);
+        setOrderTryonUrl(response?.order?.tryon_url);
+      }
+    } catch (error) {}
+  };
   useEffect(() => {
     if (orderTryonUrl === null || orderTryonUrl === " ") {
       const id: any = setInterval(getOrdersInterval(orderId), 5000);
@@ -70,6 +84,8 @@ const ImageUploader = ({
     }
   }, [orderTryonUrl]);
 
+  useEffect(() => {}, []);
+
   console.log(loader, "loaderloader", orderTryonUrl);
 
   return (
@@ -78,21 +94,26 @@ const ImageUploader = ({
         <div className=" ">
           {!imageUrl && (
             <>
-              <h6 className=" text-center">
-                <AiOutlineCloudUpload size={40} color="var(--iconColor)" />
-              </h6>
-              <div className=" fw-semibold btn btn-primary  overflow-hidden position-relative">
-                <Button disabled={selectedModalId ? false : true}>
-                  Upload File
-                  <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleImageUpload}
-                    ref={fileInputRef}
-                    className="image_uploaded"
-                  />
-                </Button>
-              </div>
+              {!loader && (
+                <>
+                  <h6 className=" text-center">
+                    <AiOutlineCloudUpload size={40} color="var(--iconColor)" />
+                  </h6>
+
+                  <div className=" fw-semibold btn btn-primary  overflow-hidden position-relative">
+                    <Button disabled={selectedModalId ? false : true}>
+                      Upload File
+                      <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageUpload}
+                        ref={fileInputRef}
+                        className="image_uploaded"
+                      />
+                    </Button>
+                  </div>
+                </>
+              )}
             </>
           )}
         </div>
@@ -111,26 +132,45 @@ const ImageUploader = ({
       {imageUrl && (
         <>
           <div className=" d-flex mt-3">
-            <Button
-              className=" me-3"
-              variant="outline-danger"
-              onClick={() => setImageUrl("")}
-            >
-              <span className=" me-2">
-                <MdRestore size={20} />
-              </span>
-              Restore
-            </Button>
-            <Button
-              variant="outline-success"
-              disabled={loader}
-              onClick={() => context?.setActiveSection("1")}
-            >
-              Go to next
-              <span className=" ms-2">
-                <FaArrowRightLong size={20} />
-              </span>
-            </Button>
+            {tryonUrlActivate ? (
+              <>
+                <Button
+                  variant="outline-success"
+                  disabled={loader}
+                  onClick={() => {
+                    handleTryonUrl(orderId);
+                  }}
+                >
+                  Submit
+                  <span className=" ms-2">
+                    <FaArrowRightLong size={20} />
+                  </span>
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  className=" me-3"
+                  variant="outline-danger"
+                  onClick={() => setImageUrl("")}
+                >
+                  <span className=" me-2">
+                    <MdRestore size={20} />
+                  </span>
+                  Restore
+                </Button>
+                <Button
+                  variant="outline-success"
+                  disabled={loader}
+                  onClick={() => context?.setActiveSection("1")}
+                >
+                  Go to next
+                  <span className=" ms-2">
+                    <FaArrowRightLong size={20} />
+                  </span>
+                </Button>
+              </>
+            )}
           </div>
         </>
       )}
